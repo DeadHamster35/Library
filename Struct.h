87aa116996406150bbc17fef94d7644199609db6
Litronom <32793715+Litronom@users.noreply.github.com>
2021-05-03 01:51:32 +0200

    More Player Struct Documentation /w Comments

--- Struct.h
+++ Struct.h
@@ -64,70 +64,70 @@ typedef struct Sus{
 
 
 typedef struct Player{
-	unsigned short   flag;
-	unsigned short   status;
-	short    rank;
-	unsigned short   point;
-	short    rap;
-	unsigned int   weapon;
-	short    item;
+	unsigned short   flag; 					//800F6690 //Player, Comp, Ghost, Etc.
+	unsigned short   status; 				//800F6692 //Kart DMA
+	short    rank; 							//800F6694 //Current Rank
+	unsigned short   point; 				//800F6696 //Current Points
+	short    rap; 							//800F6698 //Current Lap
+	unsigned int   weapon; 					//800F669C //Item Use & Item Hit Activators
+	short    item; 							//800F66A0 //Current Item
 
-	float  position[3];
-	float  old_position[3];
-    	short  direction[3];
-	float  velocity[3];
-	short    link_doshin;
-	short    kage_ang;
-	short  	 handling_flag; //800F6AD4 // Control Flags & Trophy Scene Effects
-	unsigned short   hit_flag;
-	short    angle_y[4];
-	short    angle_z[4];
-	float  up_vector[3];
-	float  gravity_xz[3];   //800F69FC
+	float  position[3];						//800F66A4 //Kart Position
+	float  old_position[3];					//800F66B0 //Kart Previous Position
+    short  direction[3];					//800F66BE //Kart Forward Direction
+	float  velocity[3];						//800F66C4 //Kart Velocity
+	short    link_doshin;					//800F66D0 //Indicator Which Thwomp Flattened Kart / Resets On Thwomp Animation Finish
+	short    kage_ang;						//800F66D2 //Kart Shadow Rotation Angle (Only In Air)
+	short  	 handling_flag; 				//800F69D4 //Control Flags & Trophy Scene Effects
+	unsigned short   hit_flag;				//800F69D6 //Hit Flag For Walls, Enemies, Thwomps, Etc.
+	short    angle_y[4];					//800F69D8 //Kart Angle Y
+	short    angle_z[4];					//800F69E0 //Kart Angle X
+	float  up_vector[3];					//800F69E0 //Kart Up Direction
+	float  gravity_xz[3];   				//800F69F4 //Kart Gravity
 
-	float    radius;
-	float    ground;
+	float    radius;						//800F6A00 //Kart Collision Radius
+	float    ground;						//800F6A04 //Kart Shadow Height Position
 
-	short   sterrangle;
-	short   shake_flag;
-	int   	nowsterr;
-	float   turbo_power;
+	short   sterrangle;						//800F6A08 //Current Steering Angle
+	short   shake_flag;						//800F6A0A //Kart Engine Shake Flag
+	int   	nowsterr;						//800F6A0C //Steering Angle Input
+	float   turbo_power;					//800F6A10 //Boost Acceleration
 
-	float	f_grip,r_grip;
-	float   force;
-	float   grip;
-	float   speed,taco;  //F6A28
+	float	f_grip,r_grip;					//800F6A14 //Kart Front Grip //800F6A18 // Kart Rear Grip
+	float   force;							//800F6A1C //Kart Current Input Force Until Max Speed
+	float   grip;							//800F6A20 //Kart Main Grip
+	float   speed,taco;  					//800F6A24 //Kart Speedometer //800F6A28 //Kart Tachometer
 
 
-	float  	accelcount;
-	float 	power_down,power;
+	float  	accelcount;						//800F6A2C //Kart Acceleration
+	float 	power_down,power;				//800F6A30 //Kart Downwards Acceleration //800F6A34 //Kart Power?
 
-	short   slipcount,sterr_count;
+	short   slipcount,sterr_count;			//800F6A38 //Kart Tumble Timer (Counter) //800F6A3A // Kart Steering Counter?
 	short   offsetsterr_flag,old_direction,thunder_timer,spin_timer,offsetsterr_timer;
-	unsigned short  talk;
+	unsigned short  talk;  					//800F6A46 //Comic FX & Unset Speech Bubbles
 	float 	offsetsterr_speed;
-	unsigned int    slip_flag;
-	short 	slipang,jumpcount;  //800F6A50 //Kart Angle //800F6A52 //Air Timer
-	short   kartpitch; //f6a64
-	short   erase;
-	short   jugemu_timer;
-	short   jugemu_flag; //800F6A5A //Lakitu Flags & Effects
-	short 	roll[4],pitch[4];
-	short   turbo_timer;
-	unsigned short  water_flag;
-	short 	bomb_timer,slipstream_timer;
-	float   slipstream_power;
+	unsigned int    slip_flag;				//800F6A4C //Kart State Effect Flags
+	short 	slipang,jumpcount;  			//800F6A50 //Kart Angle //800F6A52 //Air Timer
+	short   kartpitch; 						//800F6A64 //Kart Pitch
+	short   erase;							//800F6A56 //Kart Transparency Effects
+	short   jugemu_timer;					//800F6A58 //Lakitu Rescue Timer
+	short   jugemu_flag; 					//800F6A5A //Lakitu Flags & Effects
+	short 	roll[4],pitch[4];				//800F6A5C //Kart Roll //800F6A64 //Kart Pitch
+	short   turbo_timer;					//800F6A5C //Boost State Timer
+	unsigned short  water_flag;				//800F6A6E //Water & Splash Flag
+	short 	bomb_timer,slipstream_timer; 	//800F6A70 //Item/Object Hit Tumble Bounce Counter //800F6A72 //Slipstream Timer
+	float   slipstream_power;				//800F6A74 //Slipstream Acceleration
 
-	float   maxadd_power;  //800F6A78
-	float   jump,jmp_acc,jmp_speed;
-	unsigned short  bump_status; //800F6A89 //Surface Check
-	short	sterracc; //////NOT USE
+	float   maxadd_power;  					//800F6A78 //Drift Turbo Acceleration
+	float   jump,jmp_acc,jmp_speed;			//800F6A7C //Jump //800F6A80 //Jump Acceleration //800F6A84 //Jump Speed
+	unsigned short  bump_status; 			//800F6A88 //Current Surface
+	short	sterracc; //////NOT USE			//800F6A8A //Steering Angle Input Short
 
-	float 	mass,gravity ;
+	float 	mass,gravity ;					//800F6A8C //Kart Mass //800F6A90 //Kart Gravity
 
-	float  power_cont,shake;
+	float  power_cont,shake;				//800F6A94 //Acceleration Control? Unsure //800F6A98 //Kart Engine Shake Height
 
-	short  hitcount; //800f6a9c
+	short  hitcount; 						//800F6A9C //Kart Bonk Timer
 
 	Bump	 bump;
 	Matrix  align2;  //800F6aE0
