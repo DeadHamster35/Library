e504a1aacf56a1957399d24f3f26d02bc88b2ba2
Kyle Kaplan <kfkaplan@astro.as.utexas.edu>
2021-05-09 01:50:23 -0700

    Added ObjectSubBehaviorTurnTarget to OKBehaviors.h

--- OKBehaviors.h
+++ OKBehaviors.h
@@ -3,3 +3,4 @@
 
 extern void ObjectBehaviorWander(OKObject* InputObject);
 extern void Misbehave(OKObject* InputObject);
+extern void ObjectSubBehaviorTurnTarget(float InputPosition[3], short InputAngle, float TargetPosition[3], short ToleranceAngle);
