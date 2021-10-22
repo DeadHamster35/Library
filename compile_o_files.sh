2cec1112c69c3ef35cd00f56eeb6f121c22f8be7
DeadHamster35 <50299127+DeadHamster35@users.noreply.github.com>
2021-10-22 01:12:44 -0400

    update Mac Build .SH file

--- compile_o_files.sh
+++ compile_o_files.sh
@@ -1,10 +1,27 @@
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "LibraryVariables.c" -o "LibraryVariables.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "MarioKart3D.c" -o "MarioKart3D.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "MarioKartObjects.c" -o "MarioKartObjects.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "MarioKartMemory.c" -o "MarioKartMemory.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "MarioKartMenu.c" -o "MarioKartMenu.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "MarioKartStats.c" -o "MarioKartStats.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "PlayerChecks.c" -o "PlayerChecks.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "PlayerEffects.c" -o "PlayerEffects.o"
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "SharedFunctions.c" -o "SharedFunctions.o" 
-mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "playerstruct.c" -o "playerstruct.o"
+#Custom Level Code
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\CustomCourses\CustomLevels.c" -o "Library\CustomCourses\CustomLevels.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\CustomCourses\CustomSurfaces.c" -o "Library\CustomCourses\CustomSurfaces.o"
+
+
+#Custom Object Code
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\CustomObjects\OKBehaviors.c" -o "Library\CustomObjects\OKBehaviors.o" 
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\CustomObjects\OKCustomObjects.c" -o "Library\CustomObjects\OKCustomObjects.o"
+
+
+#Music and Sound Code
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\MusicSound\CustomSounds.c" -o "Library\MusicSound\CustomSounds.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "Library\MusicSound\MusicControl.c" -o "Library\MusicSound\MusicControl.o"
+
+
+#Player Code
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\Player\MarioKartStats.c" -o "library\Player\MarioKartStats.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\Player\PlayerChecks.c" -o "library\Player\PlayerChecks.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\Player\PlayerEffects.c" -o "library\Player\PlayerEffects.o"
+
+
+#Generic Code
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\LibraryVariables.c" -o "library\LibraryVariables.o" 
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\MarioKart3D.c" -o "library\MarioKart3D.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\MarioKartMenu.c" -o "library\MarioKartMenu.o" 
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\MarioKartObjects.c" -o "library\MarioKartObjects.o"
+mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c "library\SharedFunctions.c" -o "library\SharedFunctions.o" 
