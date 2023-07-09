6ecf98113b1e287fb22a7dcd8958ec5a2a22e66d
DeadHamster35 <50299127+DeadHamster35@users.noreply.github.com>
2023-07-09 00:24:20 -0400

    Fix header offset
    Header offset to ok_HeaderROM

--- CustomCourses/CustomLevels.c
+++ CustomCourses/CustomLevels.c
@@ -1773,7 +1773,7 @@ void loadHeaderOffsets()
 {
 	// Load the offsets to the custom course headers.
 	*targetAddress = (long)&ok_HeaderOffsets;
-	*sourceAddress = 0xBE9178;
+	*sourceAddress = ok_HeaderROM;
 	dataLength = 0x3C00;
 	runDMA();
 }
